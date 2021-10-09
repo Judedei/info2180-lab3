4af56cc2f8ed91d3b4aeb72cb1eb54b2d63cda42
Juddy-Shae Wallace <Jasmine101wallace@gmail.com>
2021-10-09 19:38:16 -0400

    Updates JavaScript.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,27 +10,32 @@ function displayBox(gridboxes)
     var grid = document.getElementById("board").childNodes;
     grid.forEach(element => { element.className = "square";});
     gridxo= grid.forEach(element => element.addEventListener("click", addXO));
+
     
     function addXO(event){
         var el = document.getElementById('div');
         var test = event.target.parentElement ;
-        var index = Array.prototype.indexOf.call(test.children, event.target);
-        
-        
-
-    
+        var index = Array.prototype.indexOf.call(test.children, event.target);    
         if (event.target !== el) {
             if (firstPlayer){
-                event.target.innerHTML = x;
-                firstPlayer = false;
-                arrGrid[index] = x;
+                if (arrGrid[index] == ""){
+                    event.target.innerHTML = x;
+                    firstPlayer = false;
+                    arrGrid[index] = x;
+                }
+                
             }else{
-                event.target.innerHTML = o;
-                arrGrid[index] = o;
-                firstPlayer = true;
-            }
-          
-          
+                if (arrGrid[index]==""){
+                    event.target.innerHTML = o;
+                    arrGrid[index] = o;
+                    firstPlayer = true;
+                }
+
+            } 
+        }
+
+        if (event.target == el) {
+            event.target =event.target
         }
     
       }
