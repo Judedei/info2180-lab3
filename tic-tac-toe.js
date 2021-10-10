3f63e119481dc64e999a77eb7a974ba2f0bab6c3
Juddy-Shae Wallace <Jasmine101wallace@gmail.com>
2021-10-10 02:00:53 -0400

    Updated Javascript.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,8 @@ var arrGrid = [[],[],[],[],[],[],[],[],[]];
 console.log(arrGrid)
 var x = "X";
 var o = "O";
+countx = 0;
+counto = 0;
 var firstPlayer = true;
 
 function displayBox(gridboxes)
@@ -13,6 +15,12 @@ function displayBox(gridboxes)
     
     grid0= grid.forEach(element => element.addEventListener("mouseover", hoverFunction));
     grid0= grid.forEach(element => element.addEventListener("mouseout", outFunction));
+
+    btn = document.querySelector('button');
+
+
+
+    var statusGet = document.getElementById("status");
     
     function hoverFunction(event){
         var el = document.getElementById('div');
@@ -40,20 +48,77 @@ function displayBox(gridboxes)
             if (firstPlayer){
                 if (arrGrid[index] == ""){
                     event.target.innerHTML = x;
+                    countx+=1
                     firstPlayer = false;
                     arrGrid[index] = x;
                     function changeColor(event) {
                         if (event.target.innerHTML == "X" && event.target.classList.contains("X")== false); {
                             event.target.classList.add("X");
                             //console.log(event.target.classList.contains("X"));
-                            
                         }                     
                     }
                     document.querySelector("div").onclick = changeColor;
-                } 
+
+                    //if-statements to validate winners.
+                    if (arrGrid[0] == "X" && arrGrid[1] == "X" && arrGrid[2] == "X"){
+                        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if(arrGrid[0] == "X" && arrGrid[3] == "X" && arrGrid[6] == "X"){
+                        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if(arrGrid[3] == "X" && arrGrid[4] == "X" && arrGrid[5] == "X"){
+                        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if(arrGrid[6] == "X" && arrGrid[7] == "X" && arrGrid[8] == "X"){
+                        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if (arrGrid[1] == "X" && arrGrid[4] == "X" && arrGrid[7] == "X"){
+                        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if (arrGrid[2] == "X" && arrGrid[5] == "X" && arrGrid[8] == "X"){
+                        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if (arrGrid[0] == "X" && arrGrid[4] == "X" && arrGrid[8] == "X"){
+                        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if (arrGrid[6] == "X" && arrGrid[4] == "X" && arrGrid[2] == "X"){
+                        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }
+                    btn.addEventListener("click", reset);
+                        function reset(e){
+                            arrGrid= [[],[],[],[],[],[],[],[],[]];
+                            document.getElementById("status").innerHTML = "Move your mouse over a square and click to play an X or an O.";
+                            event.target.innerHTML="";
+                            firstPlayer = true;
+                            statusGet.classList.remove("you-won");
+                            statusGet.classList.remove("before");
+                            statusGet.classList.remove("after");
+                        }
+                    //O's Start HERE
+                    
+                    
+                } //end of 
             }else{
                 if (arrGrid[index]==""){
                     event.target.innerHTML = o;
+                    counto += 1;
                     arrGrid[index] = o;
                     firstPlayer = true;
                     function changeColor(event) {
@@ -63,15 +128,69 @@ function displayBox(gridboxes)
                         
                       }
                       document.querySelector("div").onclick = changeColor;
+                      btn.addEventListener("click", reset);
+
+                      function reset(e){
+                          arrGrid= [[],[],[],[],[],[],[],[],[]];
+                          document.getElementById("status").innerHTML = "Move your mouse over a square and click to play an X or an O.";
+                          event.target.innerHTML="";
+                          firstPlayer = true;
+                          statusGet.classList.remove("you-won");
+                          statusGet.classList.remove("before");
+                          statusGet.classList.add("after");
+                          
+                      }
+                    if (arrGrid[0] == "O" && arrGrid[1] == "O" && arrGrid[2] == "O"){
+                        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if(arrGrid[0] == "O" && arrGrid[3] == "O" && arrGrid[6] == "O"){
+                        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if(arrGrid[3] == "O" && arrGrid[4] == "O" && arrGrid[5] == "O"){
+                        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if(arrGrid[6] == "O" && arrGrid[7] == "O" && arrGrid[8] == "O"){
+                        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if (arrGrid[1] == "O" && arrGrid[4] == "O" && arrGrid[7] == "O"){
+                        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if (arrGrid[2] == "O" && arrGrid[5] == "O" && arrGrid[8] == "O"){
+                        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if (arrGrid[0] == "O" && arrGrid[4] == "O" && arrGrid[8] == "O"){
+                        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");
+                    }else if (arrGrid[6] == "O" && arrGrid[4] == "O" && arrGrid[2] == "O"){
+                        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+                        statusGet.classList.add("you-won");
+                        statusGet.classList.add("before");
+                        statusGet.classList.add("after");      
+                }
                 }
             } 
-        }
+        }//end of first if.
        
       }
-        //const listing = event.target;
-        //listing.textContent =x; 
-        
-    };
+            
+
+            
+        };
+    
 
 
     
